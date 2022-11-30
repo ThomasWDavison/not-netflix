2b064acf5ea971660a9f2b6b84c55c413ab1611c
Thomas Davison <43893274+ThomasWDavison@users.noreply.github.com>
2022-11-30 16:31:18 +0000

    add index & test video playing

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/index")
+def index():
+    user = {'username': 'Thomas', 'password': 'mypassword'}
+    return render_template('index.html', title='Home', user=user)
+
+@app.route("/videos")
+def display_videos():
+    return render_template('videos.html', title='videos')
